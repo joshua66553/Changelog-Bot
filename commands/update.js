04fb81fae4e64143deef8a00ef4eaf6885440947
joshua66553 <joshua66553@gmail.com>
2022-02-22 02:24:33 +0000

    Update the update.js

--- commands/update.js
+++ commands/update.js
@@ -7,7 +7,6 @@ module.exports = {
     async execute(client, message, args, Discord, con) {
         const per = config.permissions.staffperms
         if (message.member.roles.cache.some(h => per.includes(h.id))) {
-
             if (!args[0]) {
                 message.channel.send("You have to input something for me to say...").then(msg => {
                     msg.delete({ timeout: 8000 }).catch(e => { if (config["bot"].debugmode) return console.log(e); })
@@ -15,19 +14,20 @@ module.exports = {
                 });
             } else {
                 const sayEmbed = new Discord.MessageEmbed()
-                    .setTitle('Server Changelog')
+                    .setTitle(`Server Changelog from ${message.author.username}`)
                     .setDescription(args.join(" "))
-                    .setThumbnail('https://cdn.discordapp.com/attachments/873028975503478824/936845464471490590/4.0logonobackground.png')
+                    .setThumbnail(`${config.bot.logo}`)
                     .setTimestamp()
+                    .setColor(`${config.bot.color}`)
                     .setFooter(`${config["bot"].copyright}`, `${message.author.displayAvatarURL({dynamic: true})}`)
-
                 message.channel.send(sayEmbed)
                 message.delete().catch(e => { if (config["bot"].debugmode) return console.log(e); });
             }
-
-
         } else {
-            message.channel.send("You don't have permission to run this command...").then(msg => msg.delete({ timeout: 9000 })).catch(e => { if (config["bot"].debugmode) return console.log(e); })
+            message.channel.send("You don't have permission to run this command...").then(msg => msg.delete({ timeout: 9000 })).catch(e => {
+                if (config["bot"].debugmode) return
+                console.log(e);
+            })
             message.delete().catch(e => { if (config["bot"].debugmode) return console.log(e); });
         }
     },
--- main.js
+++ main.js
@@ -2,26 +2,37 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 const config = require('./config');
 const fs = require('fs');
+const signale = require('signale');
 
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 client.on('ready', () => {
-    console.log('Logged in as ' + client.user.tag)
-    setInterval(async() => {
-        const guild = await client.guilds.fetch(`${config.bot.guild}`, true, true);
-        const newActivity = `over ${guild.memberCount - 7} members.`;
-        client.user.setActivity(newActivity, { type: 'WATCHING' });
-    }, 10000);
+    try {
+        signale.success('Logged in as ' + client.user.tag)
+    } catch {}
+    if (config.presence_config.use_mem === true) {
+        setInterval(async() => {
+            const guild = await client.guilds.fetch(`${config.bot.guild}`, true, true);
+            const newActivity = `over ${guild.memberCount - 7} members.`;
+            client.user.setActivity(newActivity, { type: 'WATCHING' });
+        }, 10000);
+    }
 })
 
-client.on("guildMemberAdd", member => {
-    setInterval(async() => {
-        const guild = await client.guilds.fetch(`${config.bot.guild}`, true, true);
-        const newActivity = `over ${guild.memberCount - 7} members.`;
-        client.user.setActivity(newActivity, { type: 'WATCHING' });
-    }, 10000);
+process.on('uncaughtException', (error) => {
+    signale.fatal(`Uncaught Exception: ${error}`)
 })
+
+if (config.presence_config.use_mem === true) {
+    client.on("guildMemberAdd", member => {
+        setInterval(async() => {
+            const guild = await client.guilds.fetch(`${config.bot.guild}`, true, true);
+            const newActivity = `over ${guild.memberCount - 7} members.`;
+            client.user.setActivity(newActivity, { type: 'WATCHING' });
+        }, 10000);
+    })
+}
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
@@ -41,6 +52,42 @@ client.on('message', message => {
         message.reply('There was an error.')
     }
 })
+
+if (config.presence_config.use_mem === false) {
+    let statuses = [{
+            activity: {
+                name: `${config["presence_config"].presname1}`,
+                type: `${config["presence_config"].prestype1}`
+            },
+            status: `${config["presence_config"].presstatus1}`
+        },
+        {
+            activity: {
+                name: `${config["presence_config"].presname2}`,
+                type: `${config["presence_config"].prestype2}`
+            },
+            status: `${config["presence_config"].presstatus2}`
+        },
+        {
+            activity: {
+                name: `${config["presence_config"].presname3}`,
+                type: `${config["presence_config"].prestype3}`
+            },
+            status: `${config["presence_config"].presstatus3}`
+        }
+    ];
+
+    let i = 0;
+    setInterval(() => {
+        let status = statuses[i];
+        if (!status) {
+            status = statuses[0];
+            i = 0;
+        }
+        client.user.setPresence(status);
+        i++;
+    }, config["presence_config"].preschangetimer);
+}
 
 client.on('error', console.error);
 client.on('warn', console.warn);
